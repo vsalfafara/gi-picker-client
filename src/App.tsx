0aa638e836a64067ebc473d56c84e2d90cdabc8f
vsalfafara <alfafara.vm@gmail.com>
2023-06-10 23:12:29 +0800

    Fixed particle options

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useCallback } from "react";
 import Particles from "react-particles";
 import type { Container, Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
-import particleConfig from "@/data/particle"
 
 function App() {
   const particlesInit = useCallback(async (engine: Engine) => await loadFull(engine), []);
@@ -18,7 +17,56 @@ function App() {
         className="z-10"
         init={particlesInit}
         loaded={particlesLoaded}
-        options={particleConfig}
+        options={{
+          particles: {
+            number: {
+              value: 100,
+            },
+            color: {
+              value: ["#31dec1", "#a37800", "#c336ff", "#53de35"]
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000"
+              },
+              polygon: {
+                nb_sides: 3
+              },
+            },
+            opacity: {
+              value: .7,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: .3,
+                sync: false
+              }
+            },
+            size: {
+              value: 4,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 5,
+                size_min: 0.1,
+                sync: false
+              }
+            },
+            move: {
+              enable: true,
+              speed: 4,
+              direction: "top-left",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+            }
+          },
+          retina_detect: true
+        }}
       />
       <div className="z-20 w-full flex flex-col">
         <div className="flex-grow">
