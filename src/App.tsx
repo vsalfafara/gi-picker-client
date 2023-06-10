2927c4ff0882e3d3d5fbfd003133be8d9129192b
vsalfafara <alfafara.vm@gmail.com>
2023-06-10 22:56:57 +0800

    Added particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import routes from "./routes"
+import { useCallback } from "react";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import particleConfig from "@/data/particle"
 
 function App() {
+  const particlesInit = useCallback(async (engine: Engine) => await loadFull(engine), []);
+  const particlesLoaded = useCallback(async (container: Container | undefined) => await console.log(container), []);
   return (
+    <>
     <div className="min-h-screen w-screen flex overflow-auto bg-gray-900">
       <div className="fixed top-0 left-0 h-screen w-screen bg-bg bg-cover bg-center opacity-40 z-0"></div>
-      <div className="z-10 w-full flex flex-col">
+      <Particles
+        id="tsparticles"
+        className="z-10"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleConfig}
+      />
+      <div className="z-20 w-full flex flex-col">
         <div className="flex-grow">
           <Routes>
             {routes.map((route, index) => <Route key={index} path={route.path} element={route.element} />)}
@@ -24,6 +39,7 @@ function App() {
         </div>
       </div>
     </div>
+    </>
   )
 }
 
